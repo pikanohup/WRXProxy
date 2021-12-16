c3d480331adbc1210fccf8f2a59710d60fb0c365
pikachu <hooraypikachu@gmail.com>
2021-12-16 15:51:11 +0800

    minor bug in shell script fixed

--- auto.sh
+++ auto.sh
@@ -5,11 +5,13 @@ npm install
 sudo apt-get install openssl
 sudo apt-get install libssl-dev
 
-certificatePath = "ssl"
+certificatePath="$PWD/ssl"
 
-if [[ ! -d "$tcertificatePath" ]]; then
-  mkdir "$tcertificatePath"
-  cd "$tcertificatePath"
+if [[ ! -d "$certificatePath" ]]; then
+  echo "$certificatePath"
+  mkdir "$certificatePath"
+  cd "$certificatePath"
+  echo "$PWD"
   openssl genrsa -out key.pem
   openssl req -new -key key.pem -out csr.pem
   openssl x509 -req -days 9999 -in csr.pem -signkey key.pem -out cert.pem
